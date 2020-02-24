0c5ddeb6c481d0a4e6168c85656268fde6774117
Fahad Abdullah <fahad@blubirdinteractive.com>
2020-02-24 14:28:16 +0600

    gmail api integration with wp_mail and contact form 7 with attachments

--- bbil-gmail-gsuit.php
+++ bbil-gmail-gsuit.php
@@ -97,3 +97,241 @@ Template Tag to send email
 function bgg_send_mail(){
 	$bgg_send_mail = BGGHelper::bgg_send_mail();
 }
+
+/*
+*/
+if(!function_exists('wp_mail')){
+    
+    function wp_mail( $to, $subject, $message, $headers = '', $attachments = array() ) {
+            // Compact the input, apply the filters, and extract them back out
+
+            /**
+             * Filters the wp_mail() arguments.
+             *
+             * @since 2.2.0
+             *
+             * @param array $args A compacted array of wp_mail() arguments, including the "to" email,
+             *                    subject, message, headers, and attachments values.
+             */
+            $atts = apply_filters( 'wp_mail', compact( 'to', 'subject', 'message', 'headers', 'attachments' ) );
+            // echo "<pre>";print_r($atts);echo "</pre>";exit();
+            if ( isset( $atts['to'] ) ) {
+                    $to = $atts['to'];
+            }
+
+
+            // if ( ! is_array( $to ) ) {
+            //         $to = explode( ',', $to );
+            // }
+
+            if ( isset( $atts['subject'] ) ) {
+                    $subject = $atts['subject'];
+            }
+
+            if ( isset( $atts['message'] ) ) {
+                    $message = $atts['message'];
+            }
+            if ( isset( $atts['headers'] ) ) {
+                    $headers = $atts['headers'];
+            }
+            // print_r($headers);exit();
+
+            if ( isset( $atts['attachments'] ) ) {
+                    $attachments = $atts['attachments'];
+            }
+
+            if ( ! is_array( $attachments ) ) {
+                    $attachments = explode( "\n", str_replace( "\r\n", "\n", $attachments ) );
+            }
+
+            // Headers
+            $cc       = array();
+            $bcc      = array();
+            $reply_to = array();
+
+            if ( empty( $headers ) ) {
+                    $headers = array();
+            } else {
+                    if ( ! is_array( $headers ) ) {
+                            // Explode the headers out, so this function can take both
+                            // string headers and an array of headers.
+                            $tempheaders = explode( "\n", str_replace( "\r\n", "\n", $headers ) );
+                    } else {
+                            $tempheaders = $headers;
+                    }
+                    $headers = array();
+
+                    // If it's actually got contents
+                    if ( ! empty( $tempheaders ) ) {
+                            // Iterate through the raw headers
+                        foreach ( (array) $tempheaders as $header )
+                        {
+                                if ( strpos( $header, ':' ) === false ) {
+                                        if ( false !== stripos( $header, 'boundary=' ) ) {
+                                                $parts    = preg_split( '/boundary=/i', trim( $header ) );
+                                                $boundary = trim( str_replace( array( "'", '"' ), '', $parts[1] ) );
+                                        }
+                                        continue;
+                                }
+	                        // Explode them out
+	                        list( $name, $content ) = explode( ':', trim( $header ), 2 );
+
+	                        // Cleanup crew
+	                        $name    = trim( $name );
+	                        $content = trim( $content );
+
+	                        switch ( strtolower( $name ) ) {
+	                        // Mainly for legacy -- process a From: header if it's there
+	                        case 'from':
+	                                $bracket_pos = strpos( $content, '<' );
+	                                if ( $bracket_pos !== false ) {
+	                                        // Text before the bracketed email is the "From" name.
+	                                        if ( $bracket_pos > 0 ) {
+	                                                $from_name = substr( $content, 0, $bracket_pos - 1 );
+	                                                $from_name = str_replace( '"', '', $from_name );
+	                                                $from_name = trim( $from_name );
+	                                        }
+
+	                                        $from_email = substr( $content, $bracket_pos + 1 );
+	                                        $from_email = str_replace( '>', '', $from_email );
+	                                        $from_email = trim( $from_email );
+
+	                                // Avoid setting an empty $from_email.
+	                                } elseif ( '' !== trim( $content ) ) {
+	                                        $from_email = trim( $content );
+	                                }
+	                                break;
+	                        case 'content-type':
+	                                if ( strpos( $content, ';' ) !== false ) {
+	                                        list( $type, $charset_content ) = explode( ';', $content );
+	                                        $content_type                   = trim( $type );
+	                                        if ( false !== stripos( $charset_content, 'charset=' ) ) {
+	                                                $charset = trim( str_replace( array( 'charset=', '"' ), '', $charset_content ) );
+	                                        } elseif ( false !== stripos( $charset_content, 'boundary=' ) ) {
+	                                                $boundary = trim( str_replace( array( 'BOUNDARY=', 'boundary=', '"' ), '', $charset_content ) );
+	                                                $charset  = '';
+	                                        }
+
+	                                // Avoid setting an empty $content_type.
+	                                } elseif ( '' !== trim( $content ) ) {
+	                                        $content_type = trim( $content );
+	                                }
+	                                break;
+	                        case 'cc':
+	                                $cc = array_merge( (array) $cc, explode( ',', $content ) );
+	                                break;
+	                        case 'bcc':
+	                                $bcc = array_merge( (array) $bcc, explode( ',', $content ) );
+	                                break;
+	                        case 'reply-to':
+	                                $reply_to = array_merge( (array) $reply_to, explode( ',', $content ) );
+	                                break;
+	                        default:
+	                                // Add it to our grand headers array
+	                                $headers[ trim( $name ) ] = trim( $content );
+	                                break;
+	                        }
+                        }
+                    }
+            }
+
+            // print_r($reply_to);exit();
+
+            try{
+				$getTokensData = BGGHelper::bggGetTokensData();
+				$getTokensData = $getTokensData['0'];
+				$token = $getTokensData->access_token;
+				$refresh_token = $getTokensData->refresh_token;
+
+				$credData = BGGHelper::bggGetCredData();
+
+				$setToken = BGGHelper::bggSetAccessToken($token,$refresh_token,$credData);
+				// echo "<pre>";print_r($setToken);echo "</pre>";exit();
+
+		        $service = new Google_Service_Gmail($setToken);
+
+		        $boundary = uniqid(rand(), true);
+		        $subjectCharset = $charset = 'utf-8';
+
+		        $from_email='mail2technerd@gmail.com';
+		        $from_name="Test";
+		        $to_email = $to;
+		        $reply_to = $reply_to;
+		        
+
+		        // $EmailBody = SendMails::encodeText($EmailBody); // Encoding special characters
+		        $EmailBody = $message; // Encoding special characters
+
+
+		        /*$strRawMessage = "From: Email <$from_email> \r\n";
+		        $strRawMessage .= "To:" .  implode(',',$to_email)  . "\r\n";
+		        if(count($cc_email)!=0){
+		            $strRawMessage .= "Cc:" .  implode(',',$cc_email)  . "\r\n";
+		        }
+		        if(count($bcc_email)!=0){
+		            $strRawMessage .= "Bcc:" .  implode(',',$bcc_email)  . "\r\n";
+		        }
+		        $strRawMessage .= 'Subject: =?utf-8?B?' . base64_encode($subject) . "?=\r\n";
+		        $strRawMessage .= "MIME-Version: 1.0\r\n";
+		        $strRawMessage .= "Content-Type: text/html; charset=utf-8\r\n";
+		        $strRawMessage .= 'Content-Transfer-Encoding: quoted-printable' . "\r\n\r\n";
+		        $strRawMessage .= "$EmailBody\r\n";*/
+
+		        $strRawMessage = "From: Email <$from_email> \r\n";
+		        $strRawMessage .= "To:" .  $to_email  . "\r\n";
+		        $strRawMessage .= "Reply-To:" .  $reply_to['0']  . "\r\n";
+		        
+		        
+
+		        $strRawMessage .= 'Subject: =?' . $subjectCharset . '?B?' . base64_encode($subject) . "?=\r\n";
+		        $strRawMessage .= 'MIME-Version: 1.0' . "\r\n";
+		        // $strRawMessage .= $headers . "\r\n";
+		        $strRawMessage .= 'Content-type: Multipart/Mixed; boundary="' . $boundary . '"' . "\r\n";
+		        $strRawMessage .= 'Content-Transfer-Encoding: quoted-printable' . "\r\n\r\n";
+
+		        if(count($attachments) !=0){ // Adding attachment (if any)
+		            $filePath = implode(',',$attachments);
+		            $finfo = finfo_open(FILEINFO_MIME_TYPE); // return mime type ala mimetype extension
+		            $mimeType = finfo_file($finfo, $filePath);
+		            $fileName = '';
+		            foreach($attachments as $attachment){
+		                $fileInfo = explode('/',$attachment);
+		                $fileName .= $extension = end($fileInfo).',';
+		            }
+		            $fileData = base64_encode(file_get_contents($filePath));
+
+		            $strRawMessage .= "\r\n--{$boundary}\r\n";
+		            $strRawMessage .= 'Content-Type: '. $mimeType .'; name="'. $fileName .'";' . "\r\n";
+		            $strRawMessage .= 'Content-ID: <' . $from_email . '>' . "\r\n";
+		            $strRawMessage .= 'Content-Description: ' . $fileName . ';' . "\r\n";
+		            $strRawMessage .= 'Content-Disposition: attachment; filename="' . $fileName . '"; size=' . filesize($filePath). ';' . "\r\n";
+		            $strRawMessage .= 'Content-Transfer-Encoding: base64' . "\r\n\r\n";
+		            $strRawMessage .= chunk_split(base64_encode(file_get_contents($filePath)), 76, "\n") . "\r\n";
+		            $strRawMessage .= '--' . $boundary . "\r\n";
+		        }
+
+		        $strRawMessage .= "\r\n--{$boundary}\r\n";
+		        $strRawMessage .= 'Content-Type: text/plain; charset=' . $charset . "\r\n";
+		        $strRawMessage .= 'Content-Transfer-Encoding: 7bit' . "\r\n\r\n";
+
+		        $strRawMessage .= "--{$boundary}\r\n";
+		        $strRawMessage .= 'Content-Type: text/html; charset=' . $charset . "\r\n";
+		        $strRawMessage .= 'Content-Transfer-Encoding: quoted-printable' . "\r\n\r\n";
+		        // $strRawMessage .= $headers . "\r\n";
+		        $strRawMessage .= $EmailBody . "\r\n";
+
+
+		        //$mime = rtrim(strtr(base64_encode($strRawMessage), '+/', '-_'), '=');
+		        $mime = strtr(base64_encode($strRawMessage), '+/', '-_');
+		        $msg = new Google_Service_Gmail_Message();
+		        $msg->setRaw($mime);
+		        $service->users_messages->send("me", $msg);
+
+		        return true;
+		    }catch (\Exception $exception){
+		        // echo $exception->getMessage();
+		        $exception->getMessage();
+		        return false;
+		    }
+    } 
+}
